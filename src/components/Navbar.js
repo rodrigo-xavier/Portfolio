8e695ce76c6f9e1efe61a31132a45db18f0cd618
Rodrigo Xavier <rodrigomatiasxavier@gmail.com>
2025-04-10 19:45:57 -0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -129,16 +129,16 @@ function NavBar() {
               </select>
             </Nav.Item>
 
-            <Nav.Item className="fork-btn">
-              <Button
-                href="https://github.com/rodrigo-xavier/Portfolio"
-                target="_blank"
-                className="fork-btn-inner"
-              >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
-              </Button>
-            </Nav.Item>
+            // <Nav.Item className="fork-btn">
+            //   <Button
+            //     href="https://github.com/rodrigo-xavier/Portfolio"
+            //     target="_blank"
+            //     className="fork-btn-inner"
+            //   >
+            //     <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+            //     <AiFillStar style={{ fontSize: "1.1em" }} />
+            //   </Button>
+            // </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
