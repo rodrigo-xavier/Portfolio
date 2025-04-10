28f321d897e69478ef268ef2a3a5704631ecbfdd
Rodrigo Xavier <rodrigomatiasxavier@gmail.com>
2025-04-10 20:20:49 -0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,10 @@ import Container from "react-bootstrap/Container";
 // import logo from "../Assets/logo.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import { CgGitFork } from "react-icons/cg";
+// import { CgGitFork } from "react-icons/cg";
 import { ImBlog } from "react-icons/im";
 import {
-  AiFillStar,
+  // AiFillStar,
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
   AiOutlineUser,
